3fc05a0b42d84a858e0248e1e4a5c5e6363ec817
Dennis290699 <trujilloadrian29@gmail.com>
2025-07-23 11:08:49 -0500

    Mejora-GUI
    Mejora en la apariencia grafica con archivos modulares y escalables usando HTML CSS Y JS

---
+++ graphic_interface/src/main/resources/static/js/script.js
@@ -0,0 +1,8 @@
+function togglePassword() {
+    const passwordInput = document.getElementById("password");
+    const toggleIcon = document.querySelector(".toggle-password");
+    const isHidden = passwordInput.type === "password";
+    passwordInput.type = isHidden ? "text" : "password";
+    toggleIcon.classList.toggle("fa-eye");
+    toggleIcon.classList.toggle("fa-eye-slash");
+}
