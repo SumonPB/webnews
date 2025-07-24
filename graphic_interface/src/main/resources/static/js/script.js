81642c80cf629a784d5a8d74ffc0912d2b3ae9e4
Dennis290699 <trujilloadrian29@gmail.com>
2025-07-24 14:05:56 -0500

    Update-GUI
    Introduced new CSS files for bills, category selection, profile configuration, admin creation, news insertion, register status, statistics, and update forms. Enhanced style-admin.css and other existing CSS files for improved layout, responsiveness, and visual consistency. Removed unused script.js and updated multiple HTML templates to integrate new styles and UI improvements.

--- graphic_interface/src/main/resources/static/js/script.js
+++
@@ -1,8 +0,0 @@
-function togglePassword() {
-    const passwordInput = document.getElementById("password");
-    const toggleIcon = document.querySelector(".toggle-password");
-    const isHidden = passwordInput.type === "password";
-    passwordInput.type = isHidden ? "text" : "password";
-    toggleIcon.classList.toggle("fa-eye");
-    toggleIcon.classList.toggle("fa-eye-slash");
-}
